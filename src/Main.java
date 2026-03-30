515d5ade2ad4d160a4d4775e8013684078b2e12c
RenatoNandes <renato.nandes@estudante.ufjf.br>
2026-03-30 08:55:54 -0300

    Instancia Menu na classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public static void main(String[] args) {
+import io.Menu;
+import sistema.Biblioteca;
+import sistema.CadastroPessoas;
+import utils.CarregadorCSV;
+import utils.InputUtils;
 
-    System.out.println("Hello World");
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+        CadastroPessoas cadastro = new CadastroPessoas();
+
+        Scanner sc = new Scanner(System.in);
+        InputUtils entrada = new InputUtils(sc);
+
+        CarregadorCSV.carregarLivros("livros.csv", biblioteca);
+
+        Menu menu = new Menu(biblioteca, cadastro, entrada, sc);
+        menu.exibir();
+    }
 }
