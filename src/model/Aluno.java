535709fb303809f04ade8816b0cb84e817ec9a66
RenatoNandes <renato.nandes@estudante.ufjf.br>
2026-03-30 03:53:38 -0300

    Cria classe Aluno

---
+++ src/model/Aluno.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Aluno extends Pessoa {
+    public Aluno(String nome, String matricula) {
+        super(nome, matricula);
+    }
+
+    @Override
+    public int getLimiteEmprestimos() {
+        return 2;
+    }
+}
