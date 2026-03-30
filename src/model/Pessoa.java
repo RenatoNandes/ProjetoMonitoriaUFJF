ec6a0a3e69af5a0c1f7975df0aff11a8316fea5c
RenatoNandes <renato.nandes@estudante.ufjf.br>
2026-03-30 03:47:01 -0300

    Cria classe Pessoa

---
+++ src/model/Pessoa.java
@@ -0,0 +1,21 @@
+package model;
+
+public abstract class Pessoa {
+    private String nome;
+    private String matricula;
+
+    public Pessoa(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public abstract int getLimiteEmprestimos();
+}
