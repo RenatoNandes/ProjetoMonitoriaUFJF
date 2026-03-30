3a14beb7366d3af4337ee3dd3a42381b2c2e0a86
RenatoNandes <renato.nandes@estudante.ufjf.br>
2026-03-30 01:25:58 -0300

    Cria classe abstrata Item

---
+++ src/model/Item.java
@@ -0,0 +1,14 @@
+package model;
+
+public abstract class Item {
+    private String titulo;
+
+    public Item(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public abstract void exibirInfo();
+}
