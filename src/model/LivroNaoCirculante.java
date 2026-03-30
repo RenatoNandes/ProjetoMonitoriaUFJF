ab6d86e10b0c9719ca328dd287fa1b3483caee19
RenatoNandes <renato.nandes@estudante.ufjf.br>
2026-03-30 07:03:07 -0300

    Refatora classe LivroNaoCirculante

--- src/model/LivroNaoCirculante.java
+++ src/model/LivroNaoCirculante.java
@@ -1,13 +1,26 @@
 package model;
 
-public class LivroNaoCirculante extends Item {
-
-    public LivroNaoCirculante(String titulo) {
-        super(titulo);
+public class LivroNaoCirculante extends Livro {
+    public LivroNaoCirculante(String titulo, String autor, String id, int ano) {
+        super(titulo, autor, id.endsWith("NC") ? id : id + "NC", ano, 0);
     }
 
     @Override
     public void exibirInfo() {
-        System.out.println("Livro apenas para consulta local: " + this.getTitulo());
+        System.out.println("Livro: " + getTitulo() +
+                " | Autor: " + getAutor() +
+                " | ID: " + getId() +
+                " | Ano: " + getAno() +
+                " | Indisponível: Livro apenas para consulta local");
+    }
+
+    @Override
+    public void emprestarPara(Pessoa pessoa) {
+        System.out.println("Este livro não pode ser emprestado, apenas consultado localmente");
+    }
+
+    @Override
+    public boolean isDisponivel() {
+        return false;
     }
 }
